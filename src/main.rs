5e51043d04bd3f40924b901cb7e84eb85ea552bd
AtriChatterjee1 <print.atri@gmail.com>
2024-06-12 03:23:33 +0530

    final added vga text buffer for hello world printing

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,13 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static GM: &[u8] = b"Good Morning!";
+static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     let vga_buffer = 0xb8000 as *mut u8;
 
-    for (i, &byte) in GM.iter().enumerate() {
+    for (i, &byte) in HELLO.iter().enumerate() {
         unsafe {
             *vga_buffer.offset(i as isize * 2) = byte;
             *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
