4d6ec50fac199160bf1ba93bcc31262002300979
AtriChatterjee1 <print.atri@gmail.com>
2024-06-14 02:18:53 +0530

    added  vga_buffer file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-// src/main.rs
+
 
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
@@ -11,20 +11,14 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::print_something();
 
     loop {}
 }
+
+mod vga_buffer;
+
 
 
