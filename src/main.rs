74648b130fdeda56bd481f51ced15d49125acb04
AtriChatterjee1 <print.atri@gmail.com>
2024-06-15 04:46:31 +0530

    added exit codes

--- src/main.rs
+++ src/main.rs
@@ -30,6 +30,22 @@ pub extern "C" fn _start() -> ! {
     loop{}
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
 mod vga_buffer;
 
 #[cfg(test)]
