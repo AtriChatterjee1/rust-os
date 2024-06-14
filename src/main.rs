3cb4c8338dfb2ebafd24d1c1ce8ad32e87ddca6e
AtriChatterjee1 <print.atri@gmail.com>
2024-06-15 03:18:05 +0530

    added testing support

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-
-
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+
+#![reexport_test_harness_main = "test_main"] // test-framework entry function
 
 use core::panic::PanicInfo;
+
 
 /// This function is called on panic.
 #[panic_handler]
@@ -14,17 +17,35 @@ fn panic(info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    // lock basically lock's the thread for this process, and blocks all other processes
-    vga_buffer::WRITER.lock().write_str("HELLO WORLD").unwrap();
-    write!(vga_buffer::WRITER.lock(), ", some numbers are {} and {}", 28 ,06).unwrap();
-    println!();
+    // use core::fmt::Write;
+    // // lock basically lock's the thread for this process, and blocks all other processes
+    // vga_buffer::WRITER.lock().write_str("HELLO WORLD").unwrap();
+    // write!(vga_buffer::WRITER.lock(), ", some numbers are {} and {}", 28 ,06).unwrap();
+    // println!();
 
     println!("Hello World{}", "!");
-    panic!("Some panic message");
+    #[cfg(test)]
+    test_main();
+    // panic!("Some panic message");    
+    loop{}
 }
 
 mod vga_buffer;
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
 
 
 
