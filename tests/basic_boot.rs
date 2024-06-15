211cadfa1df6105360f0cda67be2a6e351939410
AtriChatterjee1 <print.atri@gmail.com>
2024-06-15 06:32:16 +0530

    added tc

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -5,6 +5,7 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use rust_os::println;
 
 #[no_mangle] 
 pub extern "C" fn _start() -> ! {
@@ -16,4 +17,10 @@ pub extern "C" fn _start() -> ! {
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     rust_os::test_panic_handler(info);
+}
+
+#[test_case]
+
+fn test_println() {
+    println!("test_println output");
 }
