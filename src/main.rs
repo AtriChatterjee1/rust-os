148b47a5d8a0633ea2d5c1b8e803bcc53251f411
AtriChatterjee1 <print.atri@gmail.com>
2024-06-15 05:37:18 +0530

    Testable trait to simply run tests

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,17 @@ use core::panic::PanicInfo;
 mod vga_buffer;
 mod serial;
 
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+    #[cfg(test)]
+    test_main();
+
+    
+    // panic!("Some panic message");    
+    loop{}
+}
+
 /// This function is called on panic.
 #[cfg(not(test))]
 #[panic_handler]
@@ -26,22 +37,6 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    // use core::fmt::Write;
-    // // lock basically lock's the thread for this process, and blocks all other processes
-    // vga_buffer::WRITER.lock().write_str("HELLO WORLD").unwrap();
-    // write!(vga_buffer::WRITER.lock(), ", some numbers are {} and {}", 28 ,06).unwrap();
-    // println!();
-
-    println!("Hello World{}", "!");
-    #[cfg(test)]
-    test_main();
-
-    
-    // panic!("Some panic message");    
-    loop{}
-}
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u32)]
@@ -58,24 +53,36 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
         port.write(exit_code as u32);
     }
 }
+pub trait Testable {
+    fn run(&self) -> ();
+}
 
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
 
 
 #[cfg(test)]
-pub fn test_runner(tests: &[&dyn Fn()]) {
+pub fn test_runner(tests: &[&dyn Testable]) {
     serial_println!("Running {} tests", tests.len());
     for test in tests {
-        test();
+        test.run(); 
     }
     exit_qemu(QemuExitCode::Success);
 }
 
 #[test_case]
-fn trivial_assertion() {
-    serial_print!("trivial assertion... ");
-    assert_eq!(0, 1);
-    serial_println!("[ok]");
+fn trivial_assertion() {  
+    assert_eq!(1, 1);
 }
+
 
 
 
