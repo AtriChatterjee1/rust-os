760eba67400fae6b19c3bd167676d5801c6930fe
AtriChatterjee1 <print.atri@gmail.com>
2024-06-19 22:11:41 +0530

    page mapping and allocating frames

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@
 #![reexport_test_harness_main = "test_main"] // test-framework entry function
 
 use core::panic::PanicInfo;
-use x86_64::PhysAddr;
+// use x86_64::PhysAddr;
 
 
 use rust_os::println;
@@ -15,30 +15,27 @@ entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
     use rust_os::memory;
-    use x86_64::{structures::paging::Translate, VirtAddr};
+    use x86_64::{structures::paging::Page, VirtAddr};
+    use rust_os::memory::BootInfoFrameAllocator;
 
     println!("Hello World{}", "!");
     rust_os::init();
+    let mut frame_allocator = unsafe {
+        BootInfoFrameAllocator::init(&boot_info.memory_map)
+    };
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = memory::EmptyFrameAllocator;
 
-    let addresses = [
-        // the identity-mapped vga buffer page
-        0xb8000,
-        // some code page
-        0x201008,
-        // some stack page
-        0x0100_0020_1a10,
-        // virtual address mapped to physical address 0
-        boot_info.physical_memory_offset,
-    ];
+    // map an unused page
+    let page = Page::containing_address(VirtAddr::new(0));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
 
-    for &address in &addresses {
-        let virt = VirtAddr::new(address);
-        let phys = mapper.translate_addr(virt);
-        println!("{:?} -> {:?}", virt, phys);
-    }
+    // write the string `New!` to the screen through the new mapping
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
+
     #[cfg(test)]
     test_main();
 
@@ -57,6 +54,8 @@ fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     rust_os::hlt_loop();
 }
+
+
 
 #[cfg(test)]
 #[panic_handler]
